8ae813a1ad5a7c268c6946c2d8b8c885b7f148a0
bilalkh12345 <75738584+bilalkh12345@users.noreply.github.com>
2020-12-19 12:15:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """
-PROJET CARGAME VERSION 1
+PROJET CARGAME VERSION FINALE DEVELOPPE PAR KHARBACH BILAL
 
 """
+from typing import BinaryIO
+
 import pygame
 pygame.init()
 gray=(119,118,110)
@@ -16,6 +18,12 @@ display_width=800
 display_height=600
 import time
 import random
+import pickle
+import pathlib
+
+
+
+
 
 
 gamedisplays=pygame.display.set_mode((display_width,display_height))
@@ -28,10 +36,11 @@ strip=pygame.image.load("strip.jpg")
 intro_background=pygame.image.load("background.jpg")
 instruction_background=pygame.image.load("background2.jpg")
 car_width=56
+file = pathlib.Path("save.pkl")
 pause=False
 
-""""Menu principal"""
 
+""""Le main menu"""
 def intro_loop():
     intro=True
     while intro:
@@ -46,12 +55,12 @@ def intro_loop():
         TextRect.center=(400,100)
         gamedisplays.blit(TextSurf,TextRect)
         button("START",150,520,100,50,green,bright_green,"play")
-        button("QUITTER",550,520,100,50,red,bright_red,"quit")
+        button("LOAD",550,520,100,50,red,bright_red,"load")
         button("INSTRUCTIONS",300,520,200,50,blue,bright_blue,"intro")
         pygame.display.update()
         clock.tick(50)
 
-""""Definir les actions"""
+""""Définition des actions du joueur"""
 
 def button(msg,x,y,w,h,ic,ac,action=None):
     mouse=pygame.mouse.get_pos()
@@ -61,10 +70,6 @@ def button(msg,x,y,w,h,ic,ac,action=None):
         if click[0]==1 and action!=None:
             if action=="play":
                 countdown()
-            elif action=="quit":
-                pygame.quit()
-                quit()
-                sys.exit()
             elif action=="intro":
                 introduction()
             elif action=="menu":
@@ -73,6 +78,8 @@ def button(msg,x,y,w,h,ic,ac,action=None):
                 paused()
             elif action=="unpause":
                 unpaused()
+            elif action=="load":
+                game_load()
 
 
     else:
@@ -82,7 +89,7 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     textrect.center=((x+(w/2)),(y+(h/2)))
     gamedisplays.blit(textsurf,textrect)
 
-""""Fonction qui définit les instructions """
+""""Définition des instructions """
 
 def introduction():
     introduction=True
@@ -106,6 +113,8 @@ def introduction():
         stextRect.center=((150),(400))
         hTextSurf,hTextRect=text_objects("DROITE : FLECHE DROITE" ,smalltext)
         hTextRect.center=((150),(450))
+        kTextSurf, kTextRect = text_objects("SAUVEGARDEMENT AUTOMATIQUE APRES CHAQUE NIVEAU", smalltext)
+        kTextRect.center = ((310), (550))
         ptextSurf,ptextRect=text_objects("P : PAUSE  ",smalltext)
         ptextRect.center=((150),(350))
         sTextSurf,sTextRect=text_objects("CONTROLS",mediumtext)
@@ -113,12 +122,15 @@ def introduction():
         gamedisplays.blit(sTextSurf,sTextRect)
         gamedisplays.blit(stextSurf,stextRect)
         gamedisplays.blit(hTextSurf,hTextRect)
+        gamedisplays.blit(kTextSurf,kTextRect)
         gamedisplays.blit(ptextSurf,ptextRect)
         button("BACK",600,450,100,50,blue,bright_blue,"menu")
         pygame.display.update()
         clock.tick(30)
 
-""""Fonction Pause"""
+
+
+""""Pause"""
 
 def paused():
     global pause
@@ -139,13 +151,14 @@ def paused():
             button("MENU PRINCIPAL",550,450,200,50,red,bright_red,"menu")
             pygame.display.update()
             clock.tick(30)
-""""Fonction Unpause pour reprendre le jeu"""
+
+""""Reprendre le jeu"""
 
 def unpaused():
     global pause
     pause=False
 
-
+"""L'arrière plan du chrono """
 def countdown_background():
     font=pygame.font.SysFont(None,25)
     x=(display_width*0.45)
@@ -175,7 +188,7 @@ def countdown_background():
     gamedisplays.blit(score,(0,30))
     button("PAUSE",650,0,150,50,blue,bright_blue,"pause")
 
-""""Fonction Countdown qui définit le chrono avant que le jeu commence """
+""""Lancement d'un chrono avant que le jeu commence """
 
 def countdown():
     countdown=True
@@ -220,7 +233,7 @@ def countdown():
             clock.tick(1)
             game_loop()
 
-""""Fonction obstacle qui définit les differentes voitures """
+""""Les différentes voitures """
 
 def obstacle(obs_startx,obs_starty,obs):
     if obs==0:
@@ -239,7 +252,7 @@ def obstacle(obs_startx,obs_starty,obs):
         obs_pic=pygame.image.load("car7.jpg")
     gamedisplays.blit(obs_pic,(obs_startx,obs_starty))
 
-""""Fonction qui définit le score """
+""""Le score """
 
 def score_system(passed,score):
     font=pygame.font.SysFont(None,25)
@@ -248,7 +261,7 @@ def score_system(passed,score):
     gamedisplays.blit(text,(0,50))
     gamedisplays.blit(score,(0,30))
 
-
+"""Police/font d'écriture"""
 def text_objects(text,font):
     textsurface=font.render(text,True,black)
     return textsurface,textsurface.get_rect()
@@ -262,12 +275,13 @@ def message_display(text):
     time.sleep(3)
     game_loop()
 
-""""Fonction crash qui affiche GAME OVER apres l'accident """
+""""Fonction d'nterruption du jeu après l'accident"""
 
 def crash():
     message_display("GAME OVER")
 
-""""Fonction qui définit le Background """
+
+""""Définition du fond d'écran """
 
 def background():
     gamedisplays.blit(backgroundpic,(0,0))
@@ -289,9 +303,12 @@ def background():
     gamedisplays.blit(strip,(680,100))
     gamedisplays.blit(strip,(680,200))
 
+"""Positionnement de la voiture """
 def car(x,y):
     gamedisplays.blit(carimg,(x,y))
 
+
+"""Coup d'envoie du jeu"""
 def game_loop():
     global pause
     x=(display_width*0.45)
@@ -316,7 +333,7 @@ def game_loop():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 quit()
-
+            #Paramétrage des buttons flèche droite et flèche gauche
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_LEFT:
                     x_change=-5
@@ -330,9 +347,12 @@ def game_loop():
         pause=True
         gamedisplays.fill(gray)
 
+        #Mouvement d'arrière plan
         rel_y=y2%backgroundpic.get_rect().width
         gamedisplays.blit(backgroundpic,(0,rel_y-backgroundpic.get_rect().width))
         gamedisplays.blit(backgroundpic,(700,rel_y-backgroundpic.get_rect().width))
+
+
         if rel_y<800:
             gamedisplays.blit(backgroundpic,(0,rel_y))
             gamedisplays.blit(backgroundpic,(700,rel_y))
@@ -353,39 +373,164 @@ def game_loop():
         y2+=obstacle_speed
 
 
-
-
         obs_starty-=(obstacle_speed/4)
         obstacle(obs_startx,obs_starty,obs)
         obs_starty+=obstacle_speed
         car(x,y)
         score_system(passed,score)
+        #Interruption du jeu
         if x>690-car_width or x<110:
             crash()
         if x>display_width-(car_width+110) or x<110:
             crash()
+        #Le jeu continue
         if obs_starty>display_height:
             obs_starty=0-obs_height
             obs_startx=random.randrange(170,(display_width-170))
             obs=random.randrange(0,7)
             passed=passed+1
             score=passed*10
+            # Paramétrage des niveaux du jeu
             if int(passed) % 10 == 0:
                 level = level + 1
                 obstacle_speed += 2
                 largetext = pygame.font.Font("freesansbold.ttf", 80)
                 textsurf, textrect = text_objects("LEVEL" + str(level), largetext)
                 textrect.center = ((display_width / 2), (display_height / 2))
+                # sauvegarde des variables niveau,score et la vitesse du voiture à nouveau
+                with open("save.pkl", "wb") as pickle_file:
+                    pickle.dump(passed, pickle_file)
+                    pickle.dump(score, pickle_file)
+                    pickle.dump(level, pickle_file)
+                    pickle.dump(obstacle_speed, pickle_file)
+                    print("sauvegarde réussi")
                 gamedisplays.blit(textsurf, textrect)
                 pygame.display.update()
                 time.sleep(3)
-
+        #Interruption du jeu
         if y<obs_starty+obs_height:
             if x > obs_startx and x < obs_startx + obs_width or x+car_width > obs_startx and x+car_width < obs_startx+obs_width:
                 crash()
         button("Pause",650,0,150,50,blue,bright_blue,"pause")
         pygame.display.update()
         clock.tick(60)
+
+""""Chargement du jeu sauvegardé """
+def game_load():
+    #Vérification si le fichier existe sinon message d'erreur
+    if file.exists():
+        with open("save.pkl", "rb") as pickle_file:
+            # On reprend la même fonction du coup d'envoi sauf que la on reccupere le niveau,score et la vitesse chargés sur save.pkl
+            global pause
+            x = (display_width * 0.45)
+            y = (display_height * 0.8)
+            x_change = 0
+            obs = 0
+            y_change = 0
+            obs_startx = random.randrange(200, (display_width - 200))
+            obs_starty = -750
+            obs_width = 56
+            obs_height = 125
+            y2 = 7
+            fps = 120
+            #Réccuperation des données sauveagrdés
+            passed = pickle.load(pickle_file)
+            score = pickle.load(pickle_file)
+            level = pickle.load(pickle_file)
+            obstacle_speed = pickle.load(pickle_file)
+
+            bumped = False
+            while not bumped:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    #Paramétrage des buttons flèche droite et flèche gauche
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                            x_change = -5
+                        if event.key == pygame.K_RIGHT:
+                            x_change = 5
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                            x_change = 0
+
+                x += x_change
+                pause = True
+                gamedisplays.fill(gray)
+
+                #Mouvement d'arrière plan
+                rel_y = y2 % backgroundpic.get_rect().width
+                gamedisplays.blit(backgroundpic, (0, rel_y - backgroundpic.get_rect().width))
+                gamedisplays.blit(backgroundpic, (700, rel_y - backgroundpic.get_rect().width))
+                if rel_y < 800:
+                    gamedisplays.blit(backgroundpic, (0, rel_y))
+                    gamedisplays.blit(backgroundpic, (700, rel_y))
+                    gamedisplays.blit(yellow_strip, (400, rel_y))
+                    gamedisplays.blit(yellow_strip, (400, rel_y + 100))
+                    gamedisplays.blit(yellow_strip, (400, rel_y + 200))
+                    gamedisplays.blit(yellow_strip, (400, rel_y + 300))
+                    gamedisplays.blit(yellow_strip, (400, rel_y + 400))
+                    gamedisplays.blit(yellow_strip, (400, rel_y + 500))
+                    gamedisplays.blit(yellow_strip, (400, rel_y - 100))
+                    gamedisplays.blit(strip, (120, rel_y - 200))
+                    gamedisplays.blit(strip, (120, rel_y + 20))
+                    gamedisplays.blit(strip, (120, rel_y + 30))
+                    gamedisplays.blit(strip, (680, rel_y - 100))
+                    gamedisplays.blit(strip, (680, rel_y + 20))
+                    gamedisplays.blit(strip, (680, rel_y + 30))
+
+                y2 += obstacle_speed
+
+                obs_starty -= (obstacle_speed / 4)
+                obstacle(obs_startx, obs_starty, obs)
+                obs_starty += obstacle_speed
+                car(x, y)
+                score_system(passed, score)
+                #Interruption du jeu
+                if x > 690 - car_width or x < 110:
+                    crash()
+                if x > display_width - (car_width + 110) or x < 110:
+                    crash()
+                #Le jeu continue
+                if obs_starty > display_height:
+                    obs_starty = 0 - obs_height
+                    obs_startx = random.randrange(170, (display_width - 170))
+                    obs = random.randrange(0, 7)
+                    passed = passed + 1
+                    score = passed * 10
+                    #Paramètage des niveaux du jeu
+                    if int(passed) % 10 == 0:
+                        level = level + 1
+                        obstacle_speed += 2
+                        largetext = pygame.font.Font("freesansbold.ttf", 80)
+                        textsurf, textrect = text_objects("LEVEL" + str(level), largetext)
+                        textrect.center = ((display_width / 2), (display_height / 2))
+                        #sauvegarde des variables niveau,score et la vitesse du voiture à nouveau
+                        with open("save.pkl", "wb") as pickle_file:
+                            pickle.dump(passed, pickle_file)
+                            pickle.dump(score, pickle_file)
+                            pickle.dump(level, pickle_file)
+                            pickle.dump(obstacle_speed, pickle_file)
+                            print("sauvegarde reussi")
+                        gamedisplays.blit(textsurf, textrect)
+                        pygame.display.update()
+                        time.sleep(3)
+                #Interruption du jeu
+                if y < obs_starty + obs_height:
+                    if x > obs_startx and x < obs_startx + obs_width or x + car_width > obs_startx and x + car_width < obs_startx + obs_width:
+                        crash()
+                button("Pause", 650, 0, 150, 50, blue, bright_blue, "pause")
+                pygame.display.update()
+                clock.tick(60)
+    #Message d'erreur
+    else:
+        largetext = pygame.font.Font("freesansbold.ttf", 20)
+        textsurf, textrect = text_objects("Vous n'avez effectué aucun sauvegardement pour l'instant",largetext)
+        textrect.center = ((display_width / 2), (display_height / 3.5))
+        gamedisplays.blit(textsurf, textrect)
+
+
 intro_loop()
 game_loop()
 pygame.quit()
